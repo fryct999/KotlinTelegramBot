981609a452fc496f7a2fea4d00a700c01990c672
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-27 03:00:58 +0300

    KTB-02 created dictionary file and print file content

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 package fryct999
 
-fun main() {
+import java.io.File
 
+fun main() {
+    val dictionaryFile = File("words.txt")
+    dictionaryFile.createNewFile()
+
+    val words = dictionaryFile.readLines()
+    words.forEach {
+        println(it)
+    }
 }

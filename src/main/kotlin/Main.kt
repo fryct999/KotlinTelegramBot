7534a52ce993f10a8a074fc40f97c1936e08a6e1
fryct999 <66903381+fryct999@users.noreply.github.com>
2025-12-28 03:43:41 +0300

    KTB-04 add Word data class; created parsing file; formatted dictionary file.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,23 @@ package fryct999
 
 import java.io.File
 
+data class Word(
+    val original: String,
+    val translate: String,
+    var correctAnswersCount: Int = 0,
+)
+
 fun main() {
     val dictionaryFile = File("words.txt")
+    val dictionary = mutableListOf<Word>()
 
-    val words = dictionaryFile.readLines()
-    words.forEach {
-        println(it)
+    val dictionaryLines = dictionaryFile.readLines()
+    dictionaryLines.forEach {
+        val line = it.split("|")
+        val word = Word(original = line[0], translate = line[1])
+        word.correctAnswersCount = line.getOrNull(2)?.toIntOrNull() ?: word.correctAnswersCount
+        dictionary.add(word)
     }
+
+    dictionary.forEach { println(it) }
 }

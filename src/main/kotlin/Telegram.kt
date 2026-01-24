1bc53f1c2acd12805cc448d2af82b603d3135d11
fryct999 <66903381+fryct999@users.noreply.github.com>
2026-01-24 06:58:40 +0300

    KTB-13 create const

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,6 +5,8 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val TELEGRAM_BASE_URL = "https://api.telegram.org"
+
 fun main(args: Array<String>) {
     val botToken = args[0]
     var updateId = 0
@@ -24,7 +26,7 @@ fun main(args: Array<String>) {
 }
 
 fun getUpdates(botToken: String, updateId: Int): String {
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+    val urlGetUpdates = "$TELEGRAM_BASE_URL/bot$botToken/getUpdates?offset=$updateId"
     val client: HttpClient = HttpClient.newBuilder().build()
     val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())

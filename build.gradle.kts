38cf01551086380b2f59fd1674ad642c3349ebd0
fryct999 <66903381+fryct999@users.noreply.github.com>
2026-02-06 06:23:38 +0300

    add build_and_publish.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     kotlin("jvm") version "2.2.20"
     kotlin("plugin.serialization") version "2.3.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    application
 }
 
 group = "fryct999"
@@ -20,4 +22,8 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(24)
+}
+
+application {
+    mainClass.set("fryct999.TelegramKt")
 }
